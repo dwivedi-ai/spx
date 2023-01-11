1feb312740a877a6a7a0f051dfd6282493ab396c
Ankit <ankitdwivedi2524@gmail.com>
2023-01-11 12:59:23 +0530

    the first commit

--- shell.c
+++ shell.c
@@ -11,6 +11,8 @@ int checkParamFile(char *);
 
 int main(int argc, char * argv[])
 {
+    // Checking if there is a file parameter when executing the program.
+    // This part of code avoids the segmentation fault due to the calling of an uninitialized pointer.
     if (!argv[1])
     {
         printf("\033[1m");
@@ -22,22 +24,35 @@ int main(int argc, char * argv[])
         printf("interpretation terminated\n");
         return 1;
     }
+
+    // Opening File
+    FILE *fptr;
+    if ((fptr = fopen(argv[1],"r")) == NULL)
+    {
+        printf("\033[1m");
+        printf("spx: ");
+        printf("\033[1;31m");
+        printf("error: ", argv[1]);
+        printf("\033[0m");
+        printf("cannot find the specified file\n");
+        printf("error: returned 1 exit status\n");
+        return 1;
+    }
+
+    // This part of code checks if the specified file is a supported .spx file
     if (checkParamFile(argv[1]))
     {
+        printf("\033[1m");
+        printf("spx: ");
         printf("\033[1;31m");
-        printf("cannot find %s: No such file or directory\n", argv[1]);
+        printf("error: ", argv[1]);
         printf("\033[0m");
+        printf("cannot read the specified file\n");
         printf("error: returned 1 exit status\n");
         return 1;
     }
     
-    FILE *fptr;
-    fptr = fopen(argv[1],"r");
-    
-    if ((fptr = fopen(argv[1], "r")) == NULL){
-       printf("An Error Occured!");
-       exit(1);
-    }
+    // Reading File
     char ch = ' ';
     while ((ch = getc(fptr)) != EOF)
     {
